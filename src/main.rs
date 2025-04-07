ff9ab374eb83b6ca3ecb90b876f21f32c225c9b6
zrygan <zr.gntn@gmail.com>
2025-04-07 20:39:49 +0800

    Refactor main function and add Circle and Emitter classes; implement structure for circle objects and their emitters

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,11 @@
-use macroquad::prelude::*;
 
-#[macroquad::main("BasicShapes")]
-async fn main() {
-    loop {
-        clear_background(RED);
+fn main() { 
+    println!("{}", my_func(None))
+}
 
-        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
-
-        draw_text("IT WORKS!", 20.0, 20.0, 30.0, DARKGRAY);
-
-        next_frame().await
+fn my_func(x: Option<i32>) -> i64 {
+    match x {
+        Some(value) => (value as i64) * 2, 
+        None => 99999 as i64,
     }
 }
